96f0f7b43820115c1586d08c3327dee5838aa4d4
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-10-03 19:57:09 +0530

    FEAT: add board member invitation API

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,5 +1,6 @@
 const Board = require("../models/Board");
 const List = require("../models/List");
+const User = require("../models/User");
 
 // Create Board
 exports.createBoard = async (req, res) => {
@@ -38,6 +39,42 @@ exports.getBoardById = async (req, res) => {
 
     const lists = await List.find({ board: board._id }).sort("position");
     res.json({ board, lists });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+exports.addMember = async (req, res) => {
+  try {
+    const { boardId } = req.params;
+    const { email } = req.body; // email of user to add
+
+    // Check if board exists and requester is owner
+    const board = await Board.findOne({
+      _id: boardId,
+      owner: req.user._id
+    });
+    if (!board) {
+      return res.status(403).json({ message: "Not authorized to add members to this board" });
+    }
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if already a member
+    if (board.members.includes(user._id)) {
+      return res.status(400).json({ message: "User is already a member of this board" });
+    }
+
+    // Add user to board members
+    board.members.push(user._id);
+    await board.save();
+
+    res.json({ message: "Member added successfully", board });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
